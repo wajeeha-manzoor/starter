7a0eaa5c06f7364277aaa89da18f465e5ecac487
wajeeha-manzoor <wajeeha.manzoor@devopsplex.com>
2021-09-07 23:02:22 +0500

    index file changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Container, Row, Col, Image } from "react-bootstrap";
+
 const Home = () => {
   return (
-    <>
-      <h1>This is my starter project.</h1>
-      <p>Own by Wajeeha.</p>
-      <p>coming soon</p>
-    </>
+    <Container
+      fluid
+      style={{
+        backgroundImage: "linear-gradient(180deg,#fff -29%,#ecf5ff 105%)",
+        padding: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "calc(93vh + 53px)",
+      }}
+    >
+      <div style={{ fontSize: "42px", fontFamily: "Raleway" }}>
+        Hello, I'm{" "}
+        <span style={{ fontFamily: "Raleway-semibold", color: "#e31b6d" }}>
+          Wajeeha Manzoor
+        </span>
+        .
+      </div>
+      <div style={{ fontSize: "42px", fontFamily: "Raleway" }}>
+        I'm a full-stack web developer
+      </div>
+    </Container>
   );
 };
 
